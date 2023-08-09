ab8f89330c0a002b0a6f834e3ae558545c3a29b3
Trang Phung T.X <trang.phung@monstar-lab.com>
2023-08-09 18:16:15 +0700

    filling in the layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+//= require jquery3
+//= require popper
+//= require bootstrap
