517f9c6f7321a29668dffef8d6396514175fcbfd
Trang Phung T.X <trang.phung@monstar-lab.com>
2023-08-10 11:45:30 +0700

    modeling user

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get :new
+    get user_new_url
     assert_response :success
   end
 end
