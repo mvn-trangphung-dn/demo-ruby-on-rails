f82f6f761c8639cb419214ca3e8229fc4799da6d
Trang Phung T.X <trang.phung@monstar-lab.com>
2023-08-16 10:11:35 +0700

    login and logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_041536) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_082327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_041536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
